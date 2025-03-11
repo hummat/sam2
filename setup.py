ed4d7e2b7170507036bf1f34f59f65a808e4419c
Matthias Humt <github.gizzard320@passmail.net>
2025-03-11 12:45:12 +0100

    Bump package version to 1.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_namespace_packages, setup
 
 # Package metadata
 NAME = "sam2"
-VERSION = "1.1.1"
+VERSION = "1.2.1"
 DESCRIPTION = "SAM 2: Segment Anything in Images and Videos"
 URL = "https://github.com/facebookresearch/sam2"
 AUTHOR = "Meta AI"
